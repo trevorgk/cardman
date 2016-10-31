9c17fea185e095ffffed1678caed4c6671173b87
trevorgk <trevor.kilvington@carsales.com.au>
2016-11-01 00:53:09 +1100

    webpack pipeline... phew

---
+++ src/ts/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {CardTable} from './components/CardTable/CardTable'
+
+console.log('here');
+
+ReactDOM.render(
+    <CardTable />,
+    document.getElementById("example")
+);
